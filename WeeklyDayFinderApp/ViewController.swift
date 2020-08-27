77a23cbe851fe308b4f65cfc0c13386641dfd566
agsaule <agsaule@hotmail.com>
2020-08-27 18:06:02 +0300

    new app from lesson 12

--- WeeklyDayFinderApp/ViewController.swift
+++ WeeklyDayFinderApp/ViewController.swift
@@ -10,11 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dayTextField: UITextField!
+    @IBOutlet weak var monthTextField: UITextField!
+    @IBOutlet weak var yearTextField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //dayTextField.delegate = self
     }
 
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func handleCalculation(){
+        
+        let calendar = Calendar.current
+        
+        var dateComponent = DateComponents()
+        
+        guard let day = dayTextField.text, let month = monthTextField.text, let year = yearTextField.text else {
+            return
+        }
+        
+        dateComponent.day = Int(day)
+        dateComponent.month = Int(month)
+        dateComponent.year = Int(year)
+        
+        guard let date = calendar.date(from: dateComponent) else {
+            print("calendar.date error")
+            return
+        }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_EN")
+        dateFormatter.dateFormat = "EEEE"
+        
+        let weekday = dateFormatter.string(from: date)
+        let capitalizedWeekday = weekday.capitalized
+        resultLabel.text = capitalizedWeekday
+        
+    }
+    
+    
+    
+    @IBAction func findWeekDayTapped(_ sender: Any) {
+        handleCalculation()
+    }
+    
+     
+    
 }
 
